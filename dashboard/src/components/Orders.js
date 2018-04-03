d46abbeff28ef9c9da9da477f9038dfa7f241002
AjK <alberto.garbui@gmail.com>
2018-04-04 00:37:59 +0200

    [Add] Daily order table

--- dashboard/src/components/Orders.js
+++ dashboard/src/components/Orders.js
@@ -64,9 +64,13 @@ const Orders = inject("ctx")(
                 const { classes, theme } = this.props,
                     roles = this.props.ctx.roles,
                     columns = [{
-                        id: 'ownerUsername',
+                        Header: 'Day',
+                        accessor: 'day',
+                        Cell: props => moment(props.value).format('DD/MM/YY')
+                    }, {
+                        id: 'ownerEmail',
                         Header: 'Owner',
-                        accessor: d => { return d.owner ? d.owner.username : '-' }
+                        accessor: d => { return d.owner ? d.owner.email : '-' }
                     }, {
                         id: 'firstCourse',
                         Header: 'First course',
@@ -94,12 +98,72 @@ const Orders = inject("ctx")(
                         sortable: false,
                         filterable: false,
                         Cell: actions
-                    }]
+                    }],
+                    dailyOrderColumns = [{
+                        accessor: 'table',
+                        Header: 'Table'
+                    }, {
+                        accessor: 'item',
+                        Header: 'Items',
+                    }, {
+                        id: 'ownerEmail',
+                        Header: 'Owner',
+                        Aggregated: row => {
+                            return "-";
+                        },
+                        accessor: d => { return d.owner ? d.owner.email : '-' }
+                    }];
+
+                const dailyOrders = [];
+
+                if (this.props.ctx.stats.ordersStats) {
+                    for (let tableName in this.props.ctx.stats.ordersStats.stats) {
+                        const table = this.props.ctx.stats.ordersStats.stats[tableName];
+                        if (table.firstCourses) {
+                            for (let key in table.firstCourses) {
+                                const fc = table.firstCourses[key];
+                                for (let i = 0; i < fc.length; i++) {
+                                    dailyOrders.push({
+                                        table: tableName,
+                                        owner: fc[i].owner,
+                                        item: fc[i].item + (fc[i].condiment ? (" - " + fc[i].condiment) : "")
+                                    })
+                                }
+                            }
+                        }
+                        if (table.secondCourses) {
+                            for (let key in table.secondCourses) {
+                                const sc = table.secondCourses[key];
+                                for (let i = 0; i < sc.length; i++) {
+                                    let obj = {
+                                        table: tableName,
+                                        owner: sc[i].owner,
+                                        item: sc[i].item
+                                    };
+                                    for (let j = 0; j < sc[i].sideDishes.length; j++) {
+                                        obj.item = obj.item + " - " + sc[i].sideDishes[j]
+                                    }
+                                    dailyOrders.push(obj);
+                                }
+                            }
+                        }
+                    }
+                }
+
                 return (
                     <Grid container direction={"column"} justify={"flex-start"} alignItems={"stretch"}>
                         <Grid item xs={12}>
-                            <h2>Daily orders stats:</h2>
-                            <p>{JSON.stringify(this.props.ctx.stats.ordersStats.stats, null, 2)}</p>
+                            <Table
+                                title="Daily orders"
+                                filters={true}
+                                errorDataText={"Error"}
+                                emptyDataText={"Daily Order not available"}
+                                options={{ pivotBy: ['table'] }}
+                                firstTimeLoading={this.props.ctx.stats.ordersStats && this.props.ctx.stats.ordersStats.stats == null}
+                                columns={dailyOrderColumns}
+                                data={dailyOrders}
+                                store={this.props.ctx.stats}
+                            />
                         </Grid>
                         <Grid item xs={12}>
                             <Table
