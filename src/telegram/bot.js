97571bd1655e42df22da0b06fe4bd81be02c95fd
AjK <alberto.garbui@gmail.com>
2018-04-05 19:57:02 +0200

    [Change] Update reminders settings

--- src/telegram/bot.js
+++ src/telegram/bot.js
@@ -160,7 +160,7 @@ function textManager(ctx) {
   if (ctx.message.text == 'ACK') {
     //user is coming back from a scene
     return;
-  } else if (ctx.message.text.toLowerCase().indexOf("menu") != -1) {
+  } else if (ctx.message.text.toLowerCase().indexOf("menu") == 0) {
     ctx.session.mainCounter = 0;
     _getDailyMenu((err, text, menu) => {
       ctx.reply(text || err, {
