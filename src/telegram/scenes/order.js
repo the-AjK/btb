a37d2155492726fd3015cdae5687d420c072038d
AjK <alberto.garbui@gmail.com>
2018-07-03 23:11:57 +0200

    [Fix] Deadline order reached message markdown

--- src/telegram/scenes/order.js
+++ src/telegram/scenes/order.js
@@ -629,7 +629,7 @@ scene.enter((ctx) => {
                     ctx.reply("Daily menu not available yet.");
                     return ctx.scene.leave();
                 } else if (moment().isAfter(dailyMenu.deadline)) {
-                    ctx.reply("🤦🏻‍♂️ Sorry bro, you are late! Come back tomorrow!", {
+                    ctx.reply("🤦🏻‍♂️ Sorry bro, *you are late!*\nCome back tomorrow!", {
                         parse_mode: "markdown"
                     });
                     return ctx.scene.leave();
