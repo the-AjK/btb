e1dc68119c908a0dcce2c8fbf860c173024d24ae
AjK <alberto.garbui@gmail.com>
2018-07-26 23:41:35 +0200

    [Test] extra skip

--- src/telegram/scenes/extra.js
+++ src/telegram/scenes/extra.js
@@ -29,9 +29,11 @@ function deleteLastMessage(ctx) {
 function textManager(ctx) {
     if (ctx.session.handleMsg === true) {
         //if the manager is handling a previous message, discarn new messages
+        console.log("extra skip")
         return;
     }
     ctx.session.handleMsg = true;
+
     ctx.replyWithChatAction(ACTIONS.TEXT_MESSAGE);
     deleteLastMessage(ctx);
 
