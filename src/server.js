b8fac072cfce91aa1965770d961aacd8580863c0
AjK <alberto.garbui@gmail.com>
2018-07-28 01:18:15 +0200

    [Fix] Static files path

--- src/server.js
+++ src/server.js
@@ -89,11 +89,11 @@ exports.init = () => {
 
     app.use(requestIp.mw());
 
-    app.use("/static", express.static(path.join(__dirname, "dashboard/build/static")));
+    app.use("/static", express.static(path.join(__dirname, "../dashboard/build/static")));
     app.use("/api", apiRouter.api);
-    app.use("/", express.static(path.join(__dirname, "dashboard/build")));
+    app.use("/", express.static(path.join(__dirname, "../dashboard/build")));
     app.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, "dashboard/build/index.html"));
+        res.sendFile(path.join(__dirname, "../dashboard/build/index.html"));
     });
 
     require("./reminder").init();
